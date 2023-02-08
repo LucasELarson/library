2679cdf34fbdfcb5157eac6149d0902af89aa825
Lucas <larsonlucas112@gmail.com>
2023-02-07 21:39:54 -0600

    added precursor to remove from array

--- script.js
+++ script.js
@@ -4,11 +4,12 @@ const library = [];
 let count = 0;
 
 class Book {
-   constructor(title, author, pages, isread) {
+   constructor(title, author, pages, isread, bookid) {
       this.title = title;
       this.author = author;
       this.pages = pages;
       this.isread = isread;
+      this.bookid = bookid;
    }
 }
 
@@ -17,25 +18,34 @@ function addBook() {
    const $title = document.getElementById("Title").value;
    const $pages = document.getElementById("Pages").value;
    const $isread = document.getElementById("IsRead").checked;
-   const newBook = new Book($title, $author, $pages, $isread);
+   const bookID = "newBook" + count;
+   const newBook = new Book($title, $author, $pages, $isread, bookID);
+
+   // assigns each book an id for the array and adds it to the array //
+
    library.push(newBook);
+
    // creates div and assigns it a class based on how many divs there are //
    const divdiv = document.createElement("div");
    const idName = "A" + count;
    divdiv.setAttribute("id", idName);
+
    // creates the title in the div //
    const divtitle = document.createElement("h5");
    divtitle.innerHTML = $title;
    divdiv.appendChild(divtitle);
+
    // adds author to the div //
    const divauthor = document.createElement("p");
    divauthor.innerHTML = $author;
    divdiv.appendChild(divauthor);
+
    // adds book length to the div //
    const divpages = document.createElement("p");
    divpages.innerHTML = $pages;
    divdiv.appendChild(divpages);
-   // says whether or not the book is read //
+
+   // says whether or not the book is read, toggleable //
    const divisread = document.createElement("p");
    divisread.innerHTML = "Read: " + $isread;
    const idRead = "B" + count;
@@ -48,11 +58,17 @@ function addBook() {
       }
    });
    divdiv.appendChild(divisread);
+
    // creates a button to remove div entirely //
    const removebutton = document.createElement("button");
    removebutton.innerText = "Remove Book";
    removebutton.addEventListener("click", function () {
-      document.getElementById(idName).remove();
+      for (let i = 0; i <= library.length; i++) {
+         if (library[i].bookid === bookID) {
+            library.slice(i, 1);
+            document.getElementById(idName).remove();
+         }
+      }
    });
    divdiv.appendChild(removebutton);
    count = count + 1;
